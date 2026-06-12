7b87fe6be6b4b8b84639a24521d1abc3984e86ff
Tobias Foster <fostertobias@gmail.com>
2026-06-12 08:44:34 +0100

    chore: add a11y eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import eslintReact from 'eslint-plugin-react'
 import stylistic from '@stylistic/eslint-plugin'
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -19,6 +20,7 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
       eslintReact.configs.flat.recommended,
+      jsxA11yPlugin.flatConfigs.recommended,
       stylistic.configs.customize({
         quotes: 'double',
       })
